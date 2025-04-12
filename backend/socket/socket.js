9ecbd04dd25acbe57f91ef541749ba0ec4e32f2d
jayesh-zip <patiljayesh1469@gmail.com>
2025-04-12 14:55:46 +0530

    change port in soket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin: "/",
+        origin: "http://localhost:8000",
         methods:['GET','POST']
     }
 })
