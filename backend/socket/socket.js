d4b487a4bda7dbbb995b1cf79a7900806007cf0a
jayesh-zip <patiljayesh1469@gmail.com>
2025-04-12 15:07:12 +0530

    change in soket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin: "http://localhost:8000",
+        origin:process.env.URL,
         methods:['GET','POST']
     }
 })
